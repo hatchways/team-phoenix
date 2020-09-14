3fc61f100229ed3690cbddce5a84a581d727b5f7
jassalss <simerpreetjassal13@gmail.com>
2020-09-13 18:17:02 -0600

    Removed unwanted black lines

--- server/app.py
+++ server/app.py
@@ -2,12 +2,9 @@ from flask import Flask
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
 
-
 app = Flask(__name__)
-
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
-
 
 app.run(port=5000)

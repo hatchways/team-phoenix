24c4f6d92df91d91368a8a43946b9a93e91d690a
jassalss <simerpreetjassal13@gmail.com>
2020-09-15 00:35:33 -0600

    updated signup page so it can make use of auth widget

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,68 +1,34 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Paper, Box, Typography, TextField, Button } from "@material-ui/core/";
-import logo from "../assets/logo.png";
-import FooterForAuth from "../compoment/FooterForAuth";
-const useStyles = makeStyles((theme) => ({
-  forPaper: {
-    margin: theme.spacing(2),
-    width: theme.spacing(50),
-    height: theme.spacing(60),
-  },
-  forOuterBox: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-}));
+import AuthWidget from "../component/AuthWidget";
+import history from "../history";
 const SignUp = () => {
-  const classes = useStyles();
-
+  let heading = "Sign up with CalendApp";
+  let btnText = "Get started";
+  const handleSubmit = (e, email) => {
+    e.preventDefault();
+    if (email.includes("gmail")) {
+      history.push({
+        pathname: "/auth-with-google",
+        state: {
+          email,
+          prompt: `The easiest way for you to sign up is with Google.This will automatically 
+            connect your calendar so you can start using CalendApp right away`,
+          noGoogleText:
+            "Prefer to create an account with a password?Click here",
+        },
+      });
+    }
+  };
   return (
-    <Box className={classes.forOuterBox} border={1}>
-      <img alt="logo" src={logo}></img>
-      <Paper className={classes.forPaper} variant="outlined" square>
-        <Box my={5}>
-          <Typography align="center" variant="h5">
-            Sign up with CalendApp
-          </Typography>
-        </Box>
-        <Box mt={3}>
-          <form>
-            <Box>
-              <Typography align="center" variant="body1" gutterBottom>
-                Enter your e-mail to get started:
-              </Typography>
-            </Box>
-            <Box p={2}>
-              <TextField
-                label="E-mail address"
-                type="search"
-                variant="outlined"
-                fullWidth
-                align="center"
-                required
-              />
-            </Box>
-            <Box align="center" my={5}>
-              <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                type="submit"
-              >
-                Get Started
-              </Button>
-            </Box>
-          </form>
-        </Box>
-        <Box borderTop={1} borderColor="grey.300">
-          <FooterForAuth />
-        </Box>
-      </Paper>
-    </Box>
+    <React.Fragment>
+      <AuthWidget
+        heading={heading}
+        btnText={btnText}
+        onSubmitHandler={handleSubmit}
+        linkText="Log In"
+        route=""
+      />
+    </React.Fragment>
   );
 };
 

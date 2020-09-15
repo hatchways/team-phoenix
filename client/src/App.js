5c43f0cf7be8c53c1c9e17901730efad6e106b21
jassalss <simerpreetjassal13@gmail.com>
2020-09-14 18:40:03 -0600

    Added theme according to app design

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
 import { BrowserRouter, Route } from "react-router-dom";
-
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { orange, green } from "@material-ui/core/colors";
+import SignUp from "./pages/SignUp";
 
 import "./App.css";
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: orange[200],
+      main: orange[500],
+      dark: orange[900],
+    },
+    secondary: {
+      light: green[200],
+      main: green[500],
+      dark: green[900],
+    },
+  },
+});
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Route path="/" component={SignUp} />
       </BrowserRouter>
     </MuiThemeProvider>
   );

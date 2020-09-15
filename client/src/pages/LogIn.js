946b9f25d3f13dbea4de5f1186958dd0ed7c4c1e
jassalss <simerpreetjassal13@gmail.com>
2020-09-15 13:31:15 -0600

    added login page

---
+++ client/src/pages/LogIn.js
@@ -0,0 +1,34 @@
+import React from "react";
+import AuthWidget from "../component/AuthWidget";
+import history from "../history";
+
+const LogIn = () => {
+  let heading = "Log into your account";
+  let btnText = "Continue";
+  const handleSubmit = (e, email) => {
+    e.preventDefault();
+    if (email.includes("gmail")) {
+      let greetings = {
+        headerText: `Welcome back`,
+        email,
+        noGoogleText: "Don't have an account?Sign up",
+        route: "sign-up",
+      };
+      localStorage.setItem("greetings", JSON.stringify(greetings));
+      history.push("/auth-with-google");
+    }
+  };
+  return (
+    <React.Fragment>
+      <AuthWidget
+        heading={heading}
+        btnText={btnText}
+        onSubmitHandler={handleSubmit}
+        linkText="Sign Up"
+        footerText="Don't have an account?"
+        route="sign-up"
+      />
+    </React.Fragment>
+  );
+};
+export default LogIn;

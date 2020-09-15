b4779c1bb714b41b239d00d46d2d6b4154db3513
jassalss <simerpreetjassal13@gmail.com>
2020-09-15 13:31:26 -0600

    added local storegae functionality

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -7,16 +7,15 @@ const SignUp = () => {
   const handleSubmit = (e, email) => {
     e.preventDefault();
     if (email.includes("gmail")) {
-      history.push({
-        pathname: "/auth-with-google",
-        state: {
-          email,
-          prompt: `The easiest way for you to sign up is with Google.This will automatically 
+      let greetings = {
+        headerText: `Hi ${email}`,
+        prompt: `The easiest way for you to sign up is with Google.This will automatically 
             connect your calendar so you can start using CalendApp right away`,
-          noGoogleText:
-            "Prefer to create an account with a password?Click here",
-        },
-      });
+        noGoogleText: "Prefer to create an account with a password?Click here",
+        route: "sign-up",
+      };
+      localStorage.setItem("greetings", JSON.stringify(greetings));
+      history.push("/auth-with-google");
     }
   };
   return (
@@ -26,7 +25,8 @@ const SignUp = () => {
         btnText={btnText}
         onSubmitHandler={handleSubmit}
         linkText="Log In"
-        route=""
+        footerText="Already have an account?"
+        route="login-in"
       />
     </React.Fragment>
   );

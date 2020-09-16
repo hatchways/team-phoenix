29e1e7fa4868611e4f924a8183d01b5a8860dc26
jassalss <simerpreetjassal13@gmail.com>
2020-09-15 19:02:04 -0600

    make changes to app.py file

--- server/app.py
+++ server/app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from api.ping_handler import ping_handler
 from api.home_handler import home_handler
+from api.auth_handler import create_auth_blueprint
+from api.after_auth_handler import create_after_Auth_blueprint
+from flask_cors import CORS
+from config import basic_auth_configSetup
 
 app = Flask(__name__)
+app.secret_key = "Simerpreet"
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+
+CORS(app, resources={
+     r"/*": {"origins": "http://localhost:3000/*"}})
+
+gauth, google = basic_auth_configSetup(app)
 
 app.register_blueprint(home_handler)
 app.register_blueprint(ping_handler)
+app.register_blueprint(create_after_Auth_blueprint(gauth, google))
+app.register_blueprint(create_auth_blueprint(gauth, google))

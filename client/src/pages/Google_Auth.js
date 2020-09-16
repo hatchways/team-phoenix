049fce6ba786dd5450c9db987df51bc23398e38e
jassalss <simerpreetjassal13@gmail.com>
2020-09-15 19:00:44 -0600

    edit google auth page

--- client/src/pages/Google_Auth.js
+++ client/src/pages/Google_Auth.js
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import SecondAuthWidget from "../component/SecondAuthWidget";
-import history from "../history";
-import { Redirect } from "react-router-dom";
 const Google_Auth = () => {
-  const [redirect, setRedirect] = useState(false);
   const onClickHandler = async () => {
     window.location.assign("http://127.0.0.1:5000/sign-in-with-google");
   };

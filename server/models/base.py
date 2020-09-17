5ddc7cfae1b1551a41d0f956b557a73f1cddd31a
jassalss <simerpreetjassal13@gmail.com>
2020-09-17 14:13:07 -0600

    added abstract method

--- server/models/base.py
+++ server/models/base.py
@@ -1,17 +1,23 @@
 import config
+import json
+from abc import ABC, abstractmethod
 
 
-class BaseModel:
+class BaseModel(ABC):
 
     """
         This will save the data contained in the inheriting classes 'data' dictionary
          into a MongoDB collection named after the class that we are saving.
     """
-    def save(self):
+   def save(self, collection_name):
         # Stored in the database
         db = config.get_db()
-        class_type = self.__class__.__name__
-        model_object = db[class_type]
-        result = model_object.insert_one(self.data)
+        collection = db[collection_name]
+        result = collection.insert_one(self.get_dic())
         if config.is_dev_environment():
             print(f'DEBUG: Saved meeting to DB {result.inserted_id}')
+
+    @abstractmethod
+    def get_dic():
+        pass
+    

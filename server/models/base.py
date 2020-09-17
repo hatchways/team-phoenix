7af9e488c5c6b535c527e8025ca7b89e15525830
jassalss <simerpreetjassal13@gmail.com>
2020-09-17 14:17:37 -0600

    added abstract method and ABC functionality

--- server/models/base.py
+++ server/models/base.py
@@ -9,15 +9,14 @@ class BaseModel(ABC):
         This will save the data contained in the inheriting classes 'data' dictionary
          into a MongoDB collection named after the class that we are saving.
     """
-   def save(self, collection_name):
+    @abstractmethod
+    def get_dic(self):
+        pass
+
+    def save(self, collection_name):
         # Stored in the database
         db = config.get_db()
         collection = db[collection_name]
         result = collection.insert_one(self.get_dic())
         if config.is_dev_environment():
             print(f'DEBUG: Saved meeting to DB {result.inserted_id}')
-
-    @abstractmethod
-    def get_dic():
-        pass
-    

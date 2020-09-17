073338255160af1912117197d02e632edcedae0a
jassalss <simerpreetjassal13@gmail.com>
2020-09-17 14:12:55 -0600

    added abstract method to availabilty

--- server/models/availability.py
+++ server/models/availability.py
@@ -1,5 +1,12 @@
-class Availability:
+import bson
+from models.base import BaseModel
+
+
+class Availability():
     def __init__(self, day, time_from, time_to):
         self.day = day
         self.time_from = time_from
         self.time_to = time_to
+
+    def get_dic(self):
+        return {"day": self.day, "time_from": self.time_from, "time_to": self.time_to}

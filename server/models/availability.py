59486115c96a7bad3bb418ab500576595d01f801
jassalss <simerpreetjassal13@gmail.com>
2020-09-17 22:03:34 -0600

    inherit from dict functionality added

--- server/models/availability.py
+++ server/models/availability.py
@@ -1,12 +1,7 @@
-import bson
 from models.base import BaseModel
 
-
-class Availability():
+class Availability(BaseModel):
     def __init__(self, day, time_from, time_to):
-        self.day = day
-        self.time_from = time_from
-        self.time_to = time_to
-
-    def get_dic(self):
-        return {"day": self.day, "time_from": self.time_from, "time_to": self.time_to}
+        self["day"] = day
+        self["time_from"] = time_from
+        self["time_to"] = time_from

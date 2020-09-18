d3895a405ba3afa41052049b0adcedd408d6ffd1
jassalss <simerpreetjassal13@gmail.com>
2020-09-17 22:04:01 -0600

    inherit from dict functionality added

--- server/models/user.py
+++ server/models/user.py
@@ -2,7 +2,6 @@ from time import gmtime, strftime
 from models.base import BaseModel
 from models.availability import Availability
 import config
-import bson
 debug = True
 
 
@@ -11,23 +10,17 @@ class User(BaseModel):
     db_users = db.users
 
     def __init__(self, userObj):
-        self.first_name = userObj.given_name
-        self.last_name = userObj.family_name
-        self.email = userObj.email
-        self.time_zone = strftime("%z", gmtime())
-        self.availability = []
-        self.unique_url = ""
+        self["first_name"] = userObj.given_name
+        self["last_name"] = userObj.given_name
+        self["email"] = userObj.email
+        self["time_zone"] = strftime("%z", gmtime())
+        self["first_name"] = userObj.given_name
+        self["availability"] = []
+        self["unique_url"] = ""
 
-    def get_dic(self):
-        my_dic = {
-            "first_name": self.first_name,
-            "last_name": self.last_name,
-            "email": self.email,
-            "time_zone": self.time_zone,
-            "availability": {f"{key}": value.get_dic() for (key, value) in enumerate(self.availability)},
-            "unique_url": self.unique_url
-        }
-        return my_dic
+    def add(self, id, val):
+        self._dict[id] = val
 
-    def user_exist(self, email):
-        return self.db_users.find_one({"email": email})
+# "availability": {f"{key}": value.get_dic() for (key, value) in enumerate(self.availability)},
+#     def user_exist(self, email):
+#         return self.db_users.find_one({"email": email})

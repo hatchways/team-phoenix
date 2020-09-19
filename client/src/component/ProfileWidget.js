5b28dc38a3f8e8a6dff828fdcb90e05a368f7da9
jonohein <jono.hein@outlook.com>
2020-09-19 16:32:41 -0700

    Futher changes, added Progress bar

--- client/src/component/ProfileWidget.js
+++ client/src/component/ProfileWidget.js
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import ProgressBar from "./ProgressBar"
 import logo from "../assets/logo.png";
 import {
     Paper,
     Box,
     Typography,
-    TextField,
     Button,
     Divider,
     MenuItem,
     FormControl,
     InputLabel,
     Select,
+    Grid,
 } from "@material-ui/core/";
 
 const useStyles = makeStyles((theme) => ({
     forPaper: {
       margin: theme.spacing(2),
-      width: theme.spacing(50),
+      width: theme.spacing(75),
       height: theme.spacing(55),
     },
     forOuterBox: {
@@ -56,45 +57,83 @@ const ProfileWidget = (props) => {
     return (
         <Box className={classes.forOuterBox}>
             <img alt="logo" src={logo}></img>
-            <Paper className={classes.forPaper} variant="outlined">
-                <Typography align="center" variant="h6">
-                    {props.heading}
-                </Typography>
+            <Paper className={classes.forPaper}  elevation={3}>
+                <Box m={3}>
+                    <Grid container alignItems="center" justify="space-evenly" spacing={4}>
+                        <Grid item xs>
+                            <Typography align="left" variant="h6">
+                                {props.heading}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs>
+                            <ProgressBar value={20} />
+                        </Grid>
+                    </Grid>
+                </Box>
                 <Divider/>
-                <Typography alight="left" variant="body1">
-                    {props.url_prompt}
-                </Typography>
-                <Typography alight="left" variant="body1">
-                    {props.timezone_prompt}
-                </Typography>
-                <FormControl variant="outlined" className={classes.formControl}>
-                    <InputLabel htmlFor="tz"></InputLabel>
-                    <Select
-                    labelId="timezone-label"
-                    id="tz"
-                    value={timezone}
-                    onChange={handleChange}
-                    label="Time Zone"
-                    >
-                    <MenuItem value={0}>
-                        <em>UTC Time</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                </FormControl>
-                <Button className={classes.forContinueButton}
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    type="submit"
-                >{props.btn_txt}</Button>
-                <Button className={classes.forSetupLater}
-                    variant="text"
-                    size="large"
-                    type="submit"
-                >{props.btn2_txt}</Button>
+                <Box m={3}>
+                    <Grid container alignItems="center">
+                        <Grid item sm>
+                            <Typography alight="left" variant="body1">
+                                {props.url_prompt}
+                            </Typography>
+                        </Grid>
+                        <Grid item sm>
+                            <Typography alight="left" variant="body1">
+                                PlaceHolder
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container alignItems="center">
+                        <Grid item sm>
+                            <Typography alight="left" variant="body1">
+                                {props.timezone_prompt}
+                            </Typography>
+                        </Grid>
+                        <Grid item sm>
+                            <FormControl variant="outlined" className={classes.formControl}>
+                                <InputLabel htmlFor="tz"></InputLabel>
+                                <Select
+                                    labelId="timezone-label"
+                                    id="tz"
+                                    value={timezone}
+                                    onChange={handleChange}
+                                    label="Time Zone">
+                                <MenuItem value={0}>
+                                    <em>UTC Time</em>
+                                </MenuItem>
+                                <MenuItem value={10}>Ten</MenuItem>
+                                <MenuItem value={20}>Twenty</MenuItem>
+                                <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container
+                        direction="column"
+                        justify="center"
+                        alignItems="center">
+                        <Grid item sm>
+                        <Button className={classes.forContinueButton}
+                            variant="contained"
+                            color="primary"
+                            size="large"
+                            type="submit">
+                            {props.btn_txt}
+                        </Button>
+                        </Grid>
+                        <Grid item sm>
+                        <Button className={classes.forSetupLater}
+                            variant="text"
+                            size="large"
+                            type="submit">
+                            {props.btn2_txt}
+                        </Button>
+                        </Grid>
+                    </Grid>
+                    
+                </Box>
             </Paper>
         </Box>
     );
---
+++ client/src/component/ProgressBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+
+import {
+    LinearProgress,
+} from "@material-ui/core/";
+
+
+const LinearProgressBar = withStyles((theme) => ({
+    root: {
+      height: 10,
+      borderRadius: 5,
+    },
+    colorPrimary: {
+      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+    },
+    bar: {
+      borderRadius: 5,
+      backgroundColor: '#fe6b00',
+    },
+}))(LinearProgress);
+
+const ProgressBar = (props) => {
+    return <LinearProgressBar variant="determinate" value={props.value}/>
+};
+
+export default ProgressBar;

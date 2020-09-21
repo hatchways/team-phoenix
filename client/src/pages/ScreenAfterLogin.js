65e27dd742e724fb8b6fba2e22dcf82e29dfeb79
jassalss <simerpreetjassal13@gmail.com>
2020-09-21 09:21:40 -0600

    update

--- client/src/pages/ScreenAfterLogin.js
+++ client/src/pages/ScreenAfterLogin.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const ScreenAfterLogin = () => {
-  return (
-    <h1>
-      This comes after login 
-    </h1>
-  );
+  return <h1>This comes after login</h1>;
 };
 
 export default ScreenAfterLogin;

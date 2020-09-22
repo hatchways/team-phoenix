8dd785971eaa77fd807a752a954c2a7d697f7b74
jassalss <simerpreetjassal13@gmail.com>
2020-09-21 22:01:47 -0600

    restore everything the way it was before

--- server/models/base.py
+++ server/models/base.py
@@ -8,22 +8,22 @@ class BaseModel(dict):
     """
         This will save the data contained in the inheriting classes 'data' dictionary
          into a MongoDB collection named after the class that we are saving.
-
         If no collection_name is provided, it is assumed the collection is named after
          the class (Meeting -> Meeting collection)
     """
-    @classmethod
-    def save(self, collection_name=None, query=None, newValues=None):
+
+    def save(self, collection_name=None):
         if collection_name is None:
             collection_name = self.__class__.__name__
         try:
             collection = config.get_db()[collection_name]
-            if not query:
+            if not hasattr(self, "_id"):
                 result = collection.insert_one(self)
             else:
-                result = collection.update_one(query, newValues)
+                result = collection.update(
+                    {"_id": ObjectId(self._id)}, self)
             if config.is_dev_environment():
-                print(f'DEBUG: Saved obj to DB {result}')
+                print(f'DEBUG: Saved obj to DB {result.inserted_id}')
             return result
         except OperationFailure as e:
             print(f"Database operation failed: {e}")

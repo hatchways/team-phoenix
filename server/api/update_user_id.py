654ec3c26d737e3d5ea4dc0e0fb7f006d7961747
jassalss <simerpreetjassal13@gmail.com>
2020-09-22 06:39:24 -0600

    rearrange the code to make if statments more consistent

--- server/api/update_user_id.py
+++ server/api/update_user_id.py
@@ -16,22 +16,22 @@ def update_user(user_id):
     It should have body.
     """
     output = dict()
-    status = 400
+    status = 422
     data = request.json
     if not user_id:
-        output["error"] = "Missing user id"
+        return jsonify({"error": "Missing user id"}), status
     if not data:
-        output["error"] = "Missing updated data"
-    else:
-        try:
-            query = {"_id": ObjectId(user_id)}
-            newValues = {"$set": data}
-            result = User.update(query, newValues)
-            if result:
-                output["success"] = f"User with id {user_id} update suceesfully"
-            else:
-                output["error"] = "Error occurred while updaing"
-        except Exception as e:
-            output['error'] = f'{e}'
-            status = 400
-    return jsonify(output), status
+        return jsonify({"error": "Missing updated data"}), status
+    try:
+        query = {"_id": ObjectId(user_id)}
+        newValues = {"$set": data}
+        result = User.update(query, newValues)
+        if result:
+            output["success"] = f"User with id {user_id} update successfully"
+        else:
+            output["error"] = "Error occurred while updating"
+        return jsonify(output), status
+    except Exception as e:
+        output['error'] = f'{e}'
+        status = 500
+        return jsonify(output), status

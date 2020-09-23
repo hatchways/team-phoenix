702dc7bf93d9caacaa0a43e91f728a8f82c11fed
jassalss <simerpreetjassal13@gmail.com>
2020-09-23 11:47:49 -0600

    added demo code

--- client/src/pages/ScreenAfterLogin.js
+++ client/src/pages/ScreenAfterLogin.js
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ScreenAfterLogin = () => {
+  useEffect(() => {
+    var urlParams = new URLSearchParams(window.location.search);
+    let token = "";
+    let email = "";
+    if (urlParams.has("token")) {
+      token = urlParams.get("token");
+    }
+    if (urlParams.has("email")) {
+      email = urlParams.get("email");
+    }
+    const fetchData = async () => {
+      const result = await fetch(
+        "http://localhost:5000/availability?day=1600882681",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            token: token,
+            email: email,
+          }),
+        }
+      );
+      console.log(await result.json());
+    };
+    if (token && email) {
+      fetchData();
+    }
+  });
   return <h1>This comes after login</h1>;
 };
 

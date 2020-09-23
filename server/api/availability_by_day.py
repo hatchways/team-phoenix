81e5cf7b9696b6ee5c8d5d88f021d2b442dc6ada
jassalss <simerpreetjassal13@gmail.com>
2020-09-23 11:47:38 -0600

    make method work with post request

--- server/api/availability_by_day.py
+++ server/api/availability_by_day.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, app, jsonify, request, session
+from flask import Blueprint, app, jsonify, request, session, Response
 from googleapiclient.discovery import build
 from oauth2client import file, client
 from google.oauth2 import service_account
@@ -6,6 +6,7 @@ import config
 from models.user import User
 from datetime import datetime
 import time
+import json
 
 
 def covert_google_day_to_date(google_day):
@@ -17,9 +18,10 @@ availability_by_day_blueprint = Blueprint(
     'availability_by_day_blueprint', __name__)
 
 
-@availability_by_day_blueprint.route('/availability', methods=["GET"])
+@availability_by_day_blueprint.route('/availability', methods=["POST"])
 def availability_by_day():
     """
+    This fucntion expects email in request body
     This function first get day (return error if day is not present)
     Then creates rfc date from day. After that It gets user's main calendar
     and find all the enents from calendar. After finding all the events it
@@ -28,8 +30,11 @@ def availability_by_day():
     output = dict()
     status = 422
     day = request.args.get('day', default="", type=str)
-    if day == "":
-        output["error"] = "Missing day"
+    data = json.loads(request.data.decode('utf-8'))
+    token = data["token"]
+    client_email = data["email"]
+    if day == "" or not token or not client_email:
+        output["error"] = "Missing day or Access Token "
     else:
         try:
             p_time = datetime.fromtimestamp(float(day))
@@ -37,9 +42,8 @@ def availability_by_day():
                 p_time.year, p_time.month, p_time.day, 00, 00, 00).isoformat()) + "Z"
             time_max = str(datetime(
                 p_time.year, p_time.month, p_time.day, 23, 59, 59).isoformat()) + "Z"
-            user_agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
             credentials = client.AccessTokenCredentials(
-                str(session.get("access_token")), 'my-user-agent')
+                token, 'my-user-agent')
             service = build('calendar', 'v3', credentials=credentials)
 
             result = service.calendarList().list().execute()
@@ -48,9 +52,10 @@ def availability_by_day():
                     "items": [{"id": calendar_id}], "timeMax": time_max}
             calendars_result = service.freebusy().query(body=body).execute()
             free_slots_arr = find_slots(
-                calendars_result["calendars"][session["profile"]["email"]]["busy"], time_min, time_max)
+                calendars_result["calendars"].get(client_email).get("busy"), time_min, time_max)
             free_slots_in_ts = convert_to_unix_time(free_slots_arr)
-            return jsonify({"result": free_slots_in_ts})
+            output["result"] = free_slots_in_ts
+            status = 200
         except Exception as e:
             output['error'] = f'{e}'
             status = 500
@@ -65,17 +70,18 @@ def find_slots(busy_time_array, day_start, day_end):
     availabile_slots = []
     counter = 0
     length = len(busy_time_array)-1
-    if day_start != busy_time_array[counter]["start"]:
-        availabile_slots.append(
-            {"start": day_start, "end": busy_time_array[counter]["start"]})
-    for b_time in busy_time_array:
-        if counter < length:
-            availabile_slots.append({"start": (b_time["end"]), "end": (
-                busy_time_array[counter+1]["start"])})
-        counter += 1
-    if day_end != busy_time_array[counter-1]["end"]:
-        availabile_slots.append(
-            {"start": busy_time_array[counter-1]["end"], "end": day_end})
+    if length > 0:
+        if day_start != busy_time_array[counter]["start"]:
+            availabile_slots.append(
+                {"start": day_start, "end": busy_time_array[counter]["start"]})
+        for b_time in busy_time_array:
+            if counter < length:
+                availabile_slots.append({"start": (b_time["end"]), "end": (
+                    busy_time_array[counter+1]["start"])})
+            counter += 1
+        if day_end != busy_time_array[counter-1]["end"]:
+            availabile_slots.append(
+                {"start": busy_time_array[counter-1]["end"], "end": day_end})
     return availabile_slots
 
 

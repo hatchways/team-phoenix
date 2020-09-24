737b7cdd4e2a19569916d271812725f09cbcad19
jassalss <simerpreetjassal13@gmail.com>
2020-09-24 15:53:25 -0600

    integrate with flask api

--- client/src/pages/ConfirmSettings.js
+++ client/src/pages/ConfirmSettings.js
@@ -1,17 +1,21 @@
 import React from "react";
-import ConfirmWidget from "../component/ConfirmWidget"
-
+import ConfirmWidget from "../component/ConfirmWidget";
+import history from "../history";
 const ConfirmSettings = () => {
-    let heading = "Your google calendar is connected!";
-    let email = "john.doe@gmail.com"
-    return (
-        <React.Fragment>
-          <ConfirmWidget
-            heading={heading}
-            email={email}
-          />
-        </React.Fragment>
-      );
+  let heading = "Your google calendar is connected!";
+  let email = localStorage.getItem("email");
+  const handleContinue = () => {
+    history.push("/availability");
+  };
+  return (
+    <React.Fragment>
+      <ConfirmWidget
+        heading={heading}
+        email={email}
+        handleContinue={handleContinue}
+      />
+    </React.Fragment>
+  );
 };
 
 export default ConfirmSettings;

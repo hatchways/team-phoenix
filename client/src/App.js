f0b704fe5d6395fe5d0b05a921331702305eb392
jassalss <simerpreetjassal13@gmail.com>
2020-09-28 16:00:46 -0600

    deleted dummy route

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import history from "./history";
 import "./App.css";
 import LogIn from "./pages/LogIn";
 import Google_Auth from "./pages/Google_Auth";
-import ScreenAfterLogin from "./pages/ScreenAfterLogin";
 import ProfileSettings from "./pages/ProfileSettings";
 import AvailabilitySettings from "./pages/AvailabilitySettings";
 import ConfirmSettings from "./pages/ConfirmSettings";
@@ -36,12 +35,10 @@ function App() {
           <Route path="/auth-with-google" exact component={Google_Auth} />
           <Route path="/sign-up" exact component={SignUp} />
           <Route path="/login-in" exact component={LogIn} />
-          <Route path="/after-login" exact component={ScreenAfterLogin} />
           <Route path="/profile_settings" exact component={ProfileSettings} />
           <Route path="/availability" exact component={AvailabilitySettings} />
           <Route path="/confirm" exact component={ConfirmSettings} />
-          <Route path="/skeleton" exact component={EventBodyComponent}/>
-
+          <Route path="/dashboard" exact component={EventBodyComponent} />
         </Switch>
       </Router>
     </MuiThemeProvider>

4250abcc54d00fca4679c82d6da4895b642897ec
jassalss <simerpreetjassal13@gmail.com>
2020-09-28 14:18:50 -0600

    added token for auth at server

--- client/src/pages/AvailabilitySettings.js
+++ client/src/pages/AvailabilitySettings.js
@@ -10,14 +10,19 @@ const AvailabilitySettings = () => {
       end_time: end,
       days: daysSelected,
     };
+    let token = localStorage.getItem("jwt_token");
     const saveUserSetting = async () => {
       let userId = localStorage.getItem("user_id");
       if (userId && unique_url) {
         const data = await fetch(`http://localhost:5000/user/${userId}`, {
           method: "POST",
-          body: JSON.stringify({ Availability, unique_url: unique_url }),
+          body: JSON.stringify({
+            data: { Availability, unique_url },
+            token,
+          }),
         });
         let result = await data.json();
+        console.log(result);
         if (result.success) {
           history.push("/after-login");
         }

bfdbc6de89d6021760708ccc85c1a7e973460a08
jassalss <simerpreetjassal13@gmail.com>
2020-09-28 17:05:24 -0600

    fixed warning

--- client/src/pages/DashBoard.js
+++ client/src/pages/DashBoard.js
@@ -2,7 +2,7 @@ import React from "react";
 import EventBodyComponent from "../component/EventBodyComponent/EventBodyComponent";
 import { saveUserDataInLocalStorage } from "../utilities/SaveTokens";
 const DashBoard = () => {
-  let userdata = saveUserDataInLocalStorage();
+  saveUserDataInLocalStorage();
   return (
     <div>
       <EventBodyComponent />

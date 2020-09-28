7db6d84cbea6c97d0e4ef57f69a7d8dc45d99e07
jassalss <simerpreetjassal13@gmail.com>
2020-09-28 16:02:55 -0600

    deleted unwanted proptypes

--- client/src/Component/NavbarComponent/NavbarComponent.js
+++ client/src/Component/NavbarComponent/NavbarComponent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
@@ -42,10 +41,6 @@ const ButtonAppBar = () => {
       </Toolbar>
     </AppBar>
   );
-};
-
-ButtonAppBar.propTypes = {
-  classes: PropTypes.object.isRequired,
 };
 
 export default ButtonAppBar;

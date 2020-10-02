3cc657eaeefc41e39f7b059f80d76ba51e830f46
jassalss <simerpreetjassal13@gmail.com>
2020-10-01 20:36:32 -0600

    replaced 45 to 60 as described in mockups

--- server/models/meeting.py
+++ server/models/meeting.py
@@ -4,16 +4,17 @@ from models.base import BaseModel
 
 class Meeting(BaseModel):
 
-    def __init__(self, user_id, 
-    name, 
-    event_type,
-    description,
-    duration
-    ):
+    def __init__(self, user_id,
+                 name,
+                 event_type,
+                 description,
+                 duration
+                 ):
         # Meetings that are not in the list below will be rejected
-        possible_meeting_lengths = [15, 30, 45]
+        possible_meeting_lengths = [15, 30, 60]
         if duration not in possible_meeting_lengths:
-            raise ValueError(f"Meeting lengths must be one of: {possible_meeting_lengths}")
+            raise ValueError(
+                f"Meeting lengths must be one of: {possible_meeting_lengths}")
 
         # Local variables
         self['user_id'] = user_id
@@ -21,7 +22,7 @@ class Meeting(BaseModel):
         self['event_type'] = event_type
         self['description'] = description
         self['duration'] = duration
-        
 
         if config.is_dev_environment():
-            print(f"DEBUG: Created meeting with {self['user_id']} for {self['duration']} minutes")
+            print(
+                f"DEBUG: Created meeting with {self['user_id']} for {self['duration']} minutes")
